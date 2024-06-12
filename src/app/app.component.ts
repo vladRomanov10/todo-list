01986b754fc8ede2de54a76c360596c1581e5db4
Vlad Romanov <vladromeur@gmail.com>
2024-06-12 15:46:21 +0500

    Создал input компонентÐ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputComponent } from "./input/input.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
