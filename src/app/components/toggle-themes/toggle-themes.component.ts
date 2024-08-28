253ff7d60bfdcf7bebbfb4fbbbc385b34589491c
Vlad Romanov <vladromeur@gmail.com>
2024-08-28 22:47:51 +0500

    Работаю над переключением тем
    Реализовал переключение темы не в компоненте, а в сервисе

--- src/app/components/toggle-themes/toggle-themes.component.ts
+++ src/app/components/toggle-themes/toggle-themes.component.ts
@@ -1,6 +1,7 @@
 import {Component, inject} from '@angular/core';
-import {Mode} from "../../types/interfaces/theme-mode";
-import {LocalStorageService} from "../../services/local-storage.service";
+
+import {AppThemeServiceService} from "../../services/app-theme-service.service";
+
 import {NgOptimizedImage} from "@angular/common";
 
 @Component({
@@ -14,47 +15,11 @@ import {NgOptimizedImage} from "@angular/common";
 })
 export class ToggleThemesComponent {
 
-  private readonly localStorageService:LocalStorageService = inject(LocalStorageService)
+  private readonly appThemeService= inject(AppThemeServiceService)
 
-  public iconSrc:string = ''
+  public iconSrc:string = './assets/images/svg/light-mode-icon.svg'
 
-  private currentMode: Mode = Mode.LIGHT
-
-  ngOnInit():void {
-    this.setMode()
-  }
-
-  toggleMode():void {
-    if (this.currentMode === Mode.LIGHT) {
-      this.updateCurrentMode(Mode.DARK)
-    } else {
-      this.updateCurrentMode(Mode.LIGHT)
-    }
-    
-    this.localStorageService.updateLS(this.localStorageService.themeModeLSKey, this.currentMode)
-  }
-
-  private setMode():void {
-    const deviceMode:MediaQueryList = window.matchMedia('(prefers-color-scheme: dark)')
-    let userMode:Mode | null = this.localStorageService.getDataFromLS(this.localStorageService.themeModeLSKey)
-    if (!userMode) {
-      deviceMode.matches ? (userMode = Mode.DARK) : (userMode = Mode.LIGHT)
-    }
-    this.updateCurrentMode(userMode)
-  }
-
-  private updateCurrentMode(theme: Mode):void {
-    document.body.classList.remove(this.currentMode)
-    this.currentMode = theme
-    document.body.classList.add(this.currentMode)
-    this.setToggleIcon()
-  }
-
-  private setToggleIcon ():void {
-    if(this.currentMode === Mode.LIGHT) {
-      this.iconSrc = './assets/images/svg/light-mode-icon.svg'
-    } else {
-      this.iconSrc = './assets/images/svg/dark-mode-icon.svg'
-    }
+  switchTheme():void {
+    this.appThemeService.switchTheme()
   }
 }
--- src/app/services/app-theme-service.service.ts
+++ src/app/services/app-theme-service.service.ts
@@ -1,9 +1,51 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {LocalStorageService} from "./local-storage.service";
+import {Mode} from "../types/interfaces/theme-mode";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppThemeServiceService {
+  private readonly localStorageService:LocalStorageService = inject(LocalStorageService)
 
-  constructor() { }
+  public iconSrc:string = ''
+
+  public currentMode: Mode = Mode.LIGHT
+
+  constructor() {
+    this.setMode()
+  }
+
+  switchTheme() {
+    if (this.currentMode === Mode.LIGHT) {
+      this.updateCurrentMode(Mode.DARK)
+    } else {
+      this.updateCurrentMode(Mode.LIGHT)
+    }
+    this.localStorageService.updateLS(this.localStorageService.themeModeLSKey, this.currentMode)
+  }
+
+  private setMode():void {
+    const deviceMode:MediaQueryList = window.matchMedia('(prefers-color-scheme: dark)')
+    let userMode:Mode | null = this.localStorageService.getDataFromLS(this.localStorageService.themeModeLSKey)
+    if (!userMode) {
+      deviceMode.matches ? (userMode = Mode.DARK) : (userMode = Mode.LIGHT)
+    }
+    this.updateCurrentMode(userMode)
+  }
+
+  private updateCurrentMode(theme: Mode):void {
+    document.body.classList.remove(this.currentMode)
+    this.currentMode = theme
+    document.body.classList.add(this.currentMode)
+    this.setToggleIcon()
+  }
+
+  private setToggleIcon ():void {
+    if(this.currentMode === Mode.LIGHT) {
+      this.iconSrc = './assets/images/svg/light-mode-icon.svg'
+    } else {
+      this.iconSrc = './assets/images/svg/dark-mode-icon.svg'
+    }
+  }
 }
